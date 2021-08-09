c5cd7013caad95903ffbfc13edc31311b45a4dd8
jojo2357 <66704796+jojo2357@users.noreply.github.com>
2021-08-08 21:42:40 -0700

    GUI part 1

--- MDRPBridge.cs
+++ MDRPBridge.cs
@@ -22,7 +22,7 @@ namespace MusicBeePlugin
 			about.Name = "MDRP Bridge";
 			about.Description = "Bridges the audio information from MusicBee to Music Discord Rich Presence";
 			about.Author = "Smaltin & jojo2357";
-			about.TargetApplication = ""; //  the name of a Plugin Storage device or panel header for a dockable panel
+			about.TargetApplication = "Panel Header Text"; //  the name of a Plugin Storage device or panel header for a dockable panel
 			about.Type = PluginType.General;
 			about.VersionMajor = 1; // your plugin version
 			about.VersionMinor = 0;
@@ -159,5 +159,40 @@ namespace MusicBeePlugin
 
 			return request;
 		}
+		
+		        //  presence of this function indicates to MusicBee that this plugin has a dockable panel. MusicBee will create the control and pass it as the panel parameter
+        //  you can add your own controls to the panel if needed
+        //  you can control the scrollable area of the panel using the mbApiInterface.MB_SetPanelScrollableArea function
+        //  to set a MusicBee header for the panel, set about.TargetApplication in the Initialise function above to the panel header text
+        public int OnDockablePanelCreated(Control panel)
+        {
+          //    return the height of the panel and perform any initialisation here
+          //    MusicBee will call panel.Dispose() when the user removes this panel from the layout configuration
+          //    < 0 indicates to MusicBee this control is resizable and should be sized to fill the panel it is docked to in MusicBee
+          //    = 0 indicates to MusicBee this control resizeable
+          //    > 0 indicates to MusicBee the fixed height for the control.Note it is recommended you scale the height for high DPI screens(create a graphics object and get the DpiY value)
+             float dpiScaling = 0;
+            using (Graphics g = panel.CreateGraphics())
+            {
+                dpiScaling = g.DpiY / 96f;
+            }
+            panel.Paint += panel_Paint;
+            return Convert.ToInt32(100 * dpiScaling);
+        }
+
+        // presence of this function indicates to MusicBee that the dockable panel created above will show menu items when the panel header is clicked
+        // return the list of ToolStripMenuItems that will be displayed
+        public List<ToolStripItem> GetHeaderMenuItems()
+        {
+            List<ToolStripItem> list = new List<ToolStripItem>();
+            list.Add(new ToolStripMenuItem("A menu item"));
+            return list;
+        }
+
+        private void panel_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.Clear(Color.Red);
+            TextRenderer.DrawText(e.Graphics, "hello", SystemFonts.CaptionFont, new Point(10, 10), Color.Blue);
+        }
 	}
 }
